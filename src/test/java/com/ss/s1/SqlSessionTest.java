d0fdfb2090a7d25838fca12cceb529a4b9b3b80d
jw-yun <yjw42>
2020-10-21 17:49:15 +0900

    setting

---
+++ src/test/java/com/ss/s1/SqlSessionTest.java
@@ -0,0 +1,22 @@
+package com.ss.s1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SqlSessionTest extends MyTestCase {
+
+	public class SqlSessionTest extends MyTestCase() {
+		
+		@autowired
+		private SqlSession sqlsession;
+	
+		@Test
+		public void test() {
+			fail("Not yet implemented");
+		}
+
+	}
+	
+	
+}
