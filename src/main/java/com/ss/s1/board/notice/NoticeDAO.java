4bbf475dc8a5f482e1518c24a87c70f07c06007d
jw-yun <yjw42>
2020-10-22 10:01:07 +0900

    add_board

---
+++ src/main/java/com/ss/s1/board/notice/NoticeDAO.java
@@ -0,0 +1,48 @@
+package com.ss.s1.board.notice;
+
+import java.util.List;
+
+import com.ss.s1.board.BoardDAO;
+import com.ss.s1.board.BoardDTO;
+import com.ss.s1.util.Pager;
+
+public class NoticeDAO implements BoardDAO {
+
+	@Override
+	public int setInsert(BoardDTO boardDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setUpdate(BoardDTO boardDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setDelete(BoardDTO boardDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<BoardDTO> getList(Pager pager) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BoardDTO getOne(BoardDTO boardDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getCount(Pager pager) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+}
---
+++ src/main/java/com/ss/s1/board/notice/NoticeDTO.java
@@ -0,0 +1,9 @@
+package com.ss.s1.board.notice;
+
+import com.ss.s1.board.BoardDTO;
+
+public class NoticeDTO extends BoardDTO{
+	
+	
+
+}
